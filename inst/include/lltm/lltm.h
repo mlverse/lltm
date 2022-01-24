19fcd7b45167249ac6288371330cc6846798995f
Daniel Falbel <dfalbel@gmail.com>
2022-01-24 16:51:25 -0300

    no longer use c++14 features.

--- inst/include/lltm/lltm.h
+++ inst/include/lltm/lltm.h
@@ -1,15 +1,11 @@
 #include <lltm/exports.h>
 
-auto handle_exceptions = [](const auto& f) {
-  return [&](auto ... params) {
-    auto ret = f(params...);
-    host_exception_handler();
-    return ret;
-  };
-};
-
 LLTM_API int _raise_exception ();
-static auto raise_exception = handle_exceptions(_raise_exception);
+inline int raise_exception () {
+  _raise_exception();
+  host_exception_handler();
+  return 1;
+}
 
 
 
